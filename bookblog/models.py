0f1d848d1f3b6664965e2aaae71d7bcaefde5718
zikza <zikzak200015@gmail.com>
2022-04-10 21:07:03 +0300

    added book model, added 2 books

--- bookblog/models.py
+++ bookblog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    pub_year = models.IntegerField(validators=[MinValueValidator(1),
+                                       MaxValueValidator(datetime.datetime.now().year)])
+    plot = models.CharField(max_length=1000)
+    image_path = models.FilePathField(path='bookblog\static\covers')
+
+    def __str__(self):
+        return self.name
+
+    def get_author(self):
+        return self.author
+
+    def get_pub_year(self):
+        return self.pub_year
+
+    def get_plot(self):
+        return self.plot
+
+    def get_image_path(self):
+        return self.image_path
