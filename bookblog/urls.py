2e8b8811f9dfa2fb438e7d6b63794c874775d101
zikza <zikzak200015@gmail.com>
2022-04-19 17:08:54 +0300

    added search realisation

--- bookblog/urls.py
+++ bookblog/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('school/', views.school, name='school'),
     path('university/', views.university, name='university'),
     path('after/', views.after, name='after'),
-    path('book_details/<book_id>', views.BookDetails.as_view(), name='book_details'),
+    path('book_details/<book_id>', views.BookDetailsView.as_view(), name='book_details'),
+    path('random_book/', views.random_book, name='random_book'),
+    path('search/', views.SearchView.as_view(), name='search'),
 ]
--- bookblog/views.py
+++ bookblog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+import random
 from .models import Book
 from django.views import View
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -9,8 +8,8 @@ def index(request):
     return render(request, 'bookblog/index.html')
 
 def school(request):
-    contact_list = Book.objects.filter(type='school')
-    paginator = Paginator(contact_list, 2)
+    book_list = Book.objects.filter(type='school')
+    paginator = Paginator(book_list, 2)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -22,12 +21,28 @@ def university(request):
 def after(request):
     return render(request, 'bookblog/after.html')
 
-class BookDetails(View):
+class BookDetailsView(View):
 
     def get(self, request, book_id):
 
         book = Book.objects.get(id=book_id)
         return render(request, 'bookblog/book_details.html', context={'book': book})
+
+def random_book(request):
+    book_amount = Book.objects.all().count()
+    random_book_id = random.randint(1, book_amount)
+    book = Book.objects.get(id = random_book_id)
+
+    return render(request, 'bookblog/book_details.html', context={'book': book})
+
+class SearchView(View):
+
+    def get(self, request):
+        query = self.request.GET.get('book_name')
+        book = Book.objects.get(name=query)
+        return render(request, 'bookblog/book_details.html', context={'book': book})
+
+
 
 
 
